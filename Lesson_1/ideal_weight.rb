d2e826c54344ae4fa25889942b3f40d9c6c7ef04
misterMK24 <aleksandr.seleznev24@gmail.com>
2020-06-23 23:09:52 +0200

    Some changes in an ideal_wieght.rb file

--- Lesson_1/ideal_weight.rb
+++ Lesson_1/ideal_weight.rb
@@ -1,10 +1,10 @@
 print "Please, enter your name: "
 person_name = gets.chomp.capitalize
 print "Please, enter your height: "
-person_height = gets.chomp.to_i
+person_height = gets.chomp.to_f
 ideal_weight = (person_height - 110) * 1.15
 if ideal_weight > 0
   puts "#{person_name}, your ideal wight is #{ideal_weight}"
 else
-  puts "You have an ideal weght already!"
+  puts "You have an ideal weight already!"
 end
