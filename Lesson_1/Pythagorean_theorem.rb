416d7286bdc84bf07542d2e78dab44fd48f47a09
misterMK24 <aleksandr.seleznev24@gmail.com>
2020-06-24 13:33:47 +0200

    correction of an issues

--- Lesson_1/Pythagorean_theorem.rb
+++ Lesson_1/Pythagorean_theorem.rb
@@ -1,57 +1,33 @@
-a, b, c = 0
-"a".upto("c") do |side|
-  print("Пожалуйста, введите сторону #{side}: ")
-  case side
-    when "a"
-      a = gets.chomp.to_f
-    when "b"
-      b = gets.chomp.to_f
-    when "c"
-      c = gets.chomp.to_f
-  end
-end
-pythagorean_statement = false
-if (a > b) && (a > c) 
-  pythagorean_statement = (a ** 2) == (b ** 2 + c ** 2)
-  case true
-    when pythagorean_statement && (b == c)
-      puts "Треугольник прямоугольный и равнобедренный!"
-    when pythagorean_statement && (b != c)
-      puts "Треугольник прямоугольный!"
-    when b == c
-      puts "Треугольник равнобедренный!"            
-    else
-      puts "Треугольник не является ни равнобедренным, ни прямоугольным, ни равносторонним!"
-  end
-elsif (b > a) && (b > c)
-  pythagorean_statement = (b ** 2) == (a ** 2 + c ** 2)
-  case true
-    when pythagorean_statement && (a == c)
-      puts "Треугольник прямоугольный и равнобедренный!"
-    when pythagorean_statement && (a != c)
-      puts "Треугольник прямоугольный!"
-    when a == c
-      puts "Треугольник равнобедренный!"            
-    else
-      puts "Треугольник не является ни равнобедренным, ни прямоугольным, ни равносторонним!"
-  end
-elsif (c > a) && (c > b)
-  pythagorean_statement = (c ** 2) == (a ** 2 + b ** 2)
-  case true
-    when pythagorean_statement && (a == b)
-      puts "Треугольник прямоугольный и равнобедренный!"
-    when pythagorean_statement && (a != b)
-      puts "Треугольник прямоугольный!"  
-    when a == b
-      puts "Треугольник равнобедренный!"            
-    else
-      puts "Треугольник не является ни равнобедренным, ни прямоугольным, ни равносторонним!"
-  end
-else
-  case true
-    when ((a == b) || (a == c)) || (b == c)
-      puts "Треугольник равнобедренный!"
-    else
-      puts "Треугольник не является ни равнобедренным, ни прямоугольным, ни равносторонним!"
-  end
+print("Пожалуйста, введите сторону a: ")
+a = gets.chomp.to_f**2
+print("Пожалуйста, введите сторону b: ")
+b = gets.chomp.to_f**2
+print("Пожалуйста, введите сторону c: ")
+c = gets.chomp.to_f**2
+arr = [a, b, c]
+max = arr.max
+min = arr.min
+two_sides_statement = (b == c) || (a == c) || (a == b)
+
+case max
+when min
+  puts "Треугольник равносторонний и равнобедренный!"
+when a
+  pythagorean_statement = (a) == (b + c)
+  puts "Треугольник прямоугольный и равнобедренный!" if (pythagorean_statement && two_sides_statement)
+  puts "Треугольник прямоугольный!" if (pythagorean_statement && not(two_sides_statement))
+  puts "Треугольник равнобедренный" if (not(pythagorean_statement) && two_sides_statement)
+  puts "Треугольник не является ни равнобедренным, ни прямоугольным, ни равносторонним!" unless (pythagorean_statement || two_sides_statement )
+when b
+  pythagorean_statement = (b) == (a + c)
+  puts "Треугольник прямоугольный и равнобедренный!" if (pythagorean_statement && two_sides_statement)
+  puts "Треугольник прямоугольный!" if (pythagorean_statement && not(two_sides_statement))
+  puts "Треугольник равнобедренный" if (not(pythagorean_statement) && two_sides_statement)
+  puts "Треугольник не является ни равнобедренным, ни прямоугольным, ни равносторонним!" unless (pythagorean_statement || two_sides_statement )
+when c
+  pythagorean_statement = (c) == (a + b)
+  puts "Треугольник прямоугольный и равнобедренный!" if (pythagorean_statement && two_sides_statement)
+  puts "Треугольник прямоугольный!" if (pythagorean_statement && not(two_sides_statement))
+  puts "Треугольник равнобедренный" if (not(pythagorean_statement) && two_sides_statement)
+  puts "Треугольник не является ни равнобедренным, ни прямоугольным, ни равносторонним!" unless (pythagorean_statement || two_sides_statement)
 end
--- Lesson_1/discriminant.rb
+++ Lesson_1/discriminant.rb
@@ -1,25 +1,23 @@
-a, b, c = 0
-"a".upto("c") do |coef|
-  print("Пожалуйста, введите коэффициент #{coef}: ")
-  case coef
-    when "a"
-      a = gets.chomp.to_f
-    when "b"
-      b = gets.chomp.to_f
-    when "c"
-      c = gets.chomp.to_f
-  end
-end
-d = (b ** 2) - (4 * a * c)
+print("Пожалуйста, введите коэффициент a: ")
+a = gets.chomp.to_f
+print("Пожалуйста, введите коэффициент b: ")
+b = gets.chomp.to_f
+print("Пожалуйста, введите коэффициент c: ")
+c = gets.chomp.to_f
+
+d = (b**2) - (4 * a * c)
+
+# а_2 не стал заносить в блок, так как он используется в двух условиях
 a_2 = a * 2
-d_sqrt = Math.sqrt(d) if d >= 0
+
 if d > 0
-  x1 = (-b + d_sqrt) / a_2
-  x2 = (-b - d_sqrt) / a_2
+  d_sqrt = Math.sqrt(d)
+  x1 = (-b + d_sqrt)/a_2
+  x2 = (-b - d_sqrt)/a_2
   puts "d = #{d}, x1 = #{x1}, x2 = #{x2}"
 elsif d == 0
-  x1 = (-b + d_sqrt) / a_2
-  puts "d = #{d}, x1 = #{x1}"
-else 
+  x1 = -b/a_2
+  puts "x1 = #{x1}"
+else
   puts "Корней нет!"
 end
